473f0b25a86dc36ea1483e0feffefb2808ad1cef
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2021-03-16 01:36:10 +0100

    Added Dapper and created DataAccess class
    Added Dapper to the project.
    Created DataAccess class that will contain all the methods to interact with the database.
    Created a method on DataAccess to get a list of customes.

---
+++ ClassicModels/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace ClassicModels
+{
+    class DataAccess
+    {
+        public List<CustomerModel> GetCustomers()
+        {
+            using (MySqlConnection connection = new MySqlConnection(Helper.CnnVal("ClassicModels")))
+            {
+                string select = "select customerNumber, customerName from customers";
+
+                return connection.Query<CustomerModel>(select).ToList();
+            }
+        }
+    }
+}
