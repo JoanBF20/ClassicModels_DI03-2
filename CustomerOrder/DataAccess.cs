f432cff143e266b879666431c485b55a542b26cd
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2021-03-16 13:41:21 +0100

    Created method to get a Product
    Created a method on DataAccess to get a specific Product GetProduct(string productCode)

--- CustomerOrder/DataAccess.cs
+++ CustomerOrder/DataAccess.cs
@@ -20,5 +20,16 @@ namespace CustomerOrder
                 return connection.Query<OrderDetailModel>(select).ToList();
             }
         }
+        public static ProductModel GetProduct(string productCode)
+        {
+            using (MySqlConnection connection = new MySqlConnection(cnnString))
+            {
+                string select = "select productName, products.productCode, image from products" +
+                    "inner join productlines on products.productLine = productlines.productLine" +
+                    "where products.productCode = '" + productCode +"'";
+
+                return connection.Query<ProductModel>(select).FirstOrDefault();
+            }
+        }
     }
 }
