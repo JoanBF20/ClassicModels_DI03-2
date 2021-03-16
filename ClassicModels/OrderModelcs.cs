8a78fca11c7030d1f71e2741dfb3448dbda3aaa2
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2021-03-16 01:55:54 +0100

    Created model for orders
    Created OrderModel class with 5 properties:
    int orderNumber
    DateTime orderDate
    DateTime requiredDate
    DateTime shippedDate
    string status

---
+++ ClassicModels/OrderModelcs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassicModels
+{
+    class OrderModelcs
+    {
+        public int orderNumber { get; set; }
+        public DateTime orderDate { get; set; }
+        public DateTime requiredDate { get; set; }
+        public DateTime shippedDate { get; set; }
+        public string status { get; set; }
+    }
+}
