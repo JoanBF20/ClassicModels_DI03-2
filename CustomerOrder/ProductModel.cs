96e8755716a077bfa00a79390fcbbfa880afbd6e
Joan Barcelo Fiol <joanbarcelo1@paucasesnovescifp.cat>
2021-03-16 04:24:39 +0100

    Created model for Products
    Created ProductModel class with 3 properties:
    string productCode
    string productName
    byte[] image

---
+++ CustomerOrder/ProductModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerOrder
+{
+    public class ProductModel
+    {
+        public string productCode { get; set; }
+        public string productName { get; set; }
+        public byte[] image { get; set; }
+    }
+}
